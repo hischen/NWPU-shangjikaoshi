5a787e2c7b18fb0fb7aba7d7509622c551bf9c14
Alexander-Chen <cx-0118@outlook.com>
2018-02-04 00:33:38 +0800

    add 2014_1416.简单计算

---
+++ NWPU_shangjikaoshi/NWPU_shangjikaoshi/2014_1416.简单计算.cpp
@@ -0,0 +1,21 @@
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+//
+
+int main()
+{
+	int a, b;
+	scanf_s("%d %d",&a,&b);
+	if (a > 0)
+	{
+		printf("%d", b);
+	}
+	else(printf("%d", a + b));
+	/*getchar;
+	getchar;
+	cin.get();
+	cin.get();*/
+	system("pause");
+	return 0;
+}
